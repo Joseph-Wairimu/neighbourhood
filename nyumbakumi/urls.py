031360cc14c3ee2439d6840aabbf1474c6088ef6
Joseph-Wairimu <josephkamore084@gmail.com>
2022-04-15 19:25:02 +0300

    import views for auth in project

--- nyumbakumi/urls.py
+++ nyumbakumi/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+from nyumba import views as v
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('nyumba.urls')),
+    path('register/', v.register, name='register'),
+    path('',include('django.contrib.auth.urls')),
 ]
