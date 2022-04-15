4fd39908128a3e89f66e710d0c2291cfc17f1d5e
Joseph-Wairimu <josephkamore084@gmail.com>
2022-04-15 21:12:48 +0300

    neighborhood class

--- nyumba/models.py
+++ nyumba/models.py
@@ -4,3 +4,12 @@ import cloudinary
 from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class NeighborHood(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    occupants_count = models.IntegerField(default=0)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    occupants = models.ManyToManyField(User, related_name='neighborhoods')
+
+    def __str__(self):
+        return self.name
