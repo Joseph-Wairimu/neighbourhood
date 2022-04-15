1bf214a0fe5791bc356e6602cf9b1f9c27e140ed
Joseph-Wairimu <josephkamore084@gmail.com>
2022-04-15 19:35:40 +0300

    add register templates

--- nyumba/views.py
+++ nyumba/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .forms import RegisterForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+
+def register(response):
+    if response.method == "POST":
+        form = RegisterForm(response.POST)
+        if form.is_valid():
+
+            form.save()
+           
+            
+            return redirect('login')
+    else:
+        form = RegisterForm()
+    return render(response, 'register/register.html', {'form': form})    
